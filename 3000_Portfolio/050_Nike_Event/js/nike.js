2f386a66aa1d4cda34fd0b33f9d49620fe2cb4ee
jihwan160 <dark160@naver.com>
2023-03-03 17:35:46 +0900

    나이키_까르띠에_이벤트

--- 3000_Portfolio/050_Nike_Event/js/nike.js
+++ 3000_Portfolio/050_Nike_Event/js/nike.js
@@ -1,8 +1,47 @@
+// Video 제어버튼 CSS
+$('figure').find('button').css({
+    position : 'absolute',
+    right : '400px',
+    top : '50px',
+    cursor : 'pointer',
+    background: 'none',
+    outline: 'none'
+});
+$('figure').find('i').css({
+    fontSize: '30px',
+    color: '#fff'
+});
+$('figure').find('button').eq(0).css({
+    right: '540px'
+});
+$('figure').find('button').eq(1).css({
+    right: '470px'
+});
 
+// Video 제어스크립트
+// const video = $('.vid video');
+// console.log(video);
+// const button = $('figure').find('button');
+// console.log(button);
 
-
-
-
+// $(button).eq(0).click(function(){
+//     video.get(0).play();
+// })
+// $(button).eq(1).click(function(){
+//     video.get(0).pause();
+// })
+// $(button).eq(2).click(function(){
+//     video.get(0).load();
+// })
+$('figure button').eq(0).click(function(){
+    $('figure').find('video').get(0).play();
+})
+$('figure button').eq(1).click(function(){
+    $('figure').find('video').get(0).pause();
+})
+$('figure button').eq(2).click(function(){
+    $('figure').find('video').get(0).load();
+})
 
 
 
---
+++ 3000_Portfolio/060_Cartier_Event/js/cartier.js
@@ -0,0 +1,12 @@
+$('.contents article').mouseenter(function(){
+    $(this).stop().animate({width:'35%'},500);
+    $(this).find('video').get(0).play();
+})
+
+$('.contents article').mouseleave(function(){
+    $(this).stop().animate({width:'12%'},500);
+    $(this).find('video').get(0).pause();
+})
+
+
+
