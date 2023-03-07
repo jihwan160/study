30c84134a3a35ff1ffa69f1c2a8fcb187e122be8
jihwan160 <dark160@naver.com>
2023-03-07 21:58:36 +0900

    메종드컨셉이랑 정리

--- 26G/js/sub_Content.js
+++ 26G/js/sub_Content.js
@@ -6,4 +6,23 @@ $(function(){
         auto: false,
         slideWidth: 1920,
     });
+
+
+    // 알림메시지 스크립트로 생성
+    $('<div class="alert"><img src="./images/icons/alert.png" alt="준비중 알림창"></div>').appendTo('body');
+    // a태그 기능 삭제
+    $('a[href="#"]').click(function(e){
+        e.preventDefault();
+        $('.alert').stop().fadeIn(500);
+
+        setTimeout(function(){
+            $('.alert').stop().fadeOut(500);
+        },1000)
+    })
+
+    // 헤더 인클루드
+    // $('#header').load('./header.html');
+
+
+
 })
